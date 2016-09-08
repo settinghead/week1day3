5124dc62c36a90ce3a46d7fb94468fa722f69c39
Xiyang Chen <settinghead@gmail.com>
2016-09-08 04:02:31 +0000

    improved

--- library.js
+++ library.js
@@ -31,13 +31,13 @@ var library = {
   printTracks: function () {
               var tracks = this.tracks;
               for (var key in tracks){
-              console.log(tracks[key].id + ": " + tracks[key].name + " by " + tracks[key].artist + " (" + tracks[key].album + ")");
+                printSingleTrack(tracks[key]);
               }
             },
   printPlaylist: function (playlistId) {
               if(this.tracks.hasOwnProperty(playlistId)){
-                var trackId = this.tracks[playlistId];
-                console.log(trackId.id + ": " + trackId.name + " by " + trackId.artist + " (" + trackId.album + ")");
+                var track = this.tracks[playlistId];
+                printSingleTrack(track);
               }else{
                 var playId = this.playlists[playlistId];
                 console.log(`${playId.id}: ${playId.name} - ${playId.tracks.length} tracks`);
@@ -45,53 +45,67 @@ var library = {
             },
 
   addTrackToPlaylist: function (trackId, playlistId) {
-              if(this.tracks.hasOwnProperty(trackId)){
-                if(this.playlists.hasOwnProperty(playlistId)){
-                  if(this.playlists[playlistId].tracks.indexOf(trackId) === -1){
+              if(this.tracks.hasOwnProperty(trackId) &&
+                 this.playlists.hasOwnProperty(playlistId) &&
+                 this.playlists[playlistId].tracks.indexOf(trackId) === -1){
                     this.playlists[playlistId].tracks.push(trackId);
                     return this.playlists[playlistId].tracks;
-                  }
-                }
               }
             },
 
   addTrack: function (name, artist, album) {
-              function uid() {
-                return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-              }
 
               var newId = uid();
-              this.tracks[newId] = new Object();
-              this.tracks[newId].id = newId;
-              this.tracks[newId].name = name;
-              this.tracks[newId].artist = artist;
-              this.tracks[newId].album = album;
+              
+              this.tracks[newId] = {
+                id: newId,
+                name: name,
+                artist: artist,
+                album: album
+              };
 
               return this.tracks;
             },
 
  addPlaylist: function (name) {
-              function uid() {
-               return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-              }
-              var newId = uid();
-              this.playlists[newId] = new Object();
 
-              this.playlists[newId].id = newId;
-              this.playlists[newId].name = name;
-              this.playlists[newId].tracks = [];
+              var newId = uid();
+              
+              this.playlists[newId] = {
+                id: newId,
+                name: name,
+                tracks: []
+              };
 
               return this.playlists;
             },
 
   printSearchResults: function(query) {
-              for (var key in this.tracks){
-                var newQuery = query.toLowerCase();
-                if(this.tracks[key].name.toLowerCase().search(newQuery) !== -1 || this.tracks[key].artist.toLowerCase().search(newQuery) !== -1 || this.tracks[key].album.toLowerCase().search(newQuery) !== -1){
-                  console.log(this.tracks[key]);
+              var newQ = query.toLowerCase();
+
+              for (var key in this.tracks) {
+                
+                var track = this.tracks[key];
+                
+                if(lowerCaseFound(track.name, newQ) || 
+                   lowerCaseFound(track.artist, newQ) ||
+                   lowerCaseFound(track.album, newQ)) {
+                  console.log(track);
                 }
               }
             }
 }
 
-console.log(library.printPlaylists());
+function lowerCaseFound(subject, term) {
+  return subject.toLowerCase().search(term) !== -1;
+}
+
+function uid() {
+  return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+}
+
+function printSingleTrack(t) {
+  console.log(t.id + ": " + t.name + " by " + t.artist + " (" + t.album + ")");
+}
+
+library.printPlaylists();
